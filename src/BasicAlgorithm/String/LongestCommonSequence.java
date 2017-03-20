1eed16627a2cc6334ac9843beac2c61ba66189be
geekieo <geekieo@hotmail.com>
2017-03-20 23:28:26 +0800

    代码调整

--- src/BasicAlgorithm/String/LongestCommonSequence.java
+++ src/BasicAlgorithm/String/LongestCommonSequence.java
@@ -1,10 +1,5 @@
 package String;
 
-import net.mindview.util.CountingGenerator;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 最长公共子序列 LCS
  * 使用二维矩阵记录相同字符邻接次序法，最后倒序查找即可
@@ -13,23 +8,34 @@ import java.util.List;
 public class LongestCommonSequence {
 
     /**
-     * 用二维数组矩阵记录二维空间次序，决定搜索的方向
+     * 如果str1[i]=str2[j],
+     * 那么
+     * 用二维数组矩阵记录空间次序，决定搜索的方向
      * @param str1
      * @param str2
      * @return
      */
-    public static int[][] getSequenceMat(char[] str1, char[] str2) {
+    public static SequenceMat[][] getSequenceMat(char[] str1, char[] str2) {
+        SequenceMat[][] mat = new SequenceMat[str1.length][str2.length];
+        for(int i = 0; i<str1.length; i++) {
+            for(int j = 0; j<str2.length; j++) {
+                if(str1[i]==str2[j]){
+                    mat[i][j].sequence=mat[i-1][j-1].sequence+1;
+                    mat[i][j].direction=1;
+                }
+            }
+        }
         return null;
     }
 
     /**
-     * 从二维矩阵中倒序搜索出最长公共子序列，并展示
+     * 从sequenceMat的direction参数中倒序搜索出最长公共子序列，并展示
      * @param sequenceMat
      * @param str1
      * @param str2
      * @return
      */
-    public static void showLCSequence(int[][]sequenceMat, char[] str1, char[] str2) {
+    public static void showLCSequence(SequenceMat[][]sequenceMat, char[] str1, char[] str2) {
 
     }
 
@@ -48,7 +54,32 @@ public class LongestCommonSequence {
          * 注意！最终结果不一定要写在 main 函数里，可以写在子函数里。
          * 枝干函数间用参数传递信息，如此保证枝干函数的次序性和关联性。
          */
-        int[][] sequenceMat = getSequenceMat(strA,strB);
+        SequenceMat[][] sequenceMat = getSequenceMat(strA,strB);
         showLCSequence(sequenceMat,strA,strB);
     }
+}
+
+class SequenceMat {
+    int sequence;//次序
+    int direction;//在二维数组中,如果横纵俩值相等，则记录邻接斜向的次序，否则记录
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+}
+
+enum Direction {
+
 }
