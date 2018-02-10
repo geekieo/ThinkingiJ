8ab8e870e0cdd6ce75f0457b646ff4fd2c7c18c8
geekieo <geekieo@hotmail.com>
2018-02-10 21:21:37 +0800

    synchronized lock

--- src/Case/LockTest.java
+++ src/Case/LockTest.java
@@ -2,11 +2,11 @@
  * Created by geekieo on 2018/2/10.
  */
 public class LockTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Lock fatMan = new Lock("food");
         Lock thinMan = new Lock("money");
-        new Thread(fatMan,"Fatter").start();
-        new Thread(thinMan,"Thinner").start();
+        new Thread(fatMan, "Fatter").start();
+        new Thread(thinMan, "Thinner").start();
     }
 }
 
@@ -16,35 +16,35 @@ class Lock implements Runnable {
     static Object money = new Object();
     private String valuables;
 
-    public Lock(String valuables){
+    public Lock(String valuables) {
         this.valuables = valuables;
     }
 
-    public void run(){
-        if(valuables=="food"){
-            while (true){
-                synchronized (food){// synchronized同步锁，锁住对象，只能一条线程用
+    public void run() {
+        if (valuables == "food") {
+            while (true) {
+                synchronized (food) {// synchronized同步锁，锁住对象，只能一条线程用
                     System.out.println(Thread.currentThread().getName()
-                            +"---get---food!");
-                    synchronized (money){
+                            + "---get---food!");
+                    synchronized (money) {
                         System.out.println(Thread.currentThread().getName()
-                                +"---get---money!");
+                                + "---get---money!");
                     }
                 }
             }
-        }else if(valuables=="money"){
+        } else if (valuables == "money") {
             try {
                 Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            while (true){
-                synchronized (money){
+            while (true) {
+                synchronized (money) {
                     System.out.println(Thread.currentThread().getName()
-                            +"---get---money!");
-                    synchronized (food){
+                            + "---get---money!");
+                    synchronized (food) {
                         System.out.println(Thread.currentThread().getName()
-                                +"---get---food!");
+                                + "---get---food!");
                     }
                 }
             }
